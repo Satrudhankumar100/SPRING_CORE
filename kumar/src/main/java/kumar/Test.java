265377a954a06fa9b741217ebc9b5e2647d0d6f2
Satrudhan Kumar <satrudhankumar678@gmail.com>
2024-04-01 14:57:20 +0530

    day_1_Ex

---
+++ kumar/src/main/java/kumar/Test.java
@@ -0,0 +1,16 @@
+package kumar;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	
+	
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("config.xml");
+		Object ob = ac.getBean("mobj");
+		
+		System.out.print(ob);
+	}
+	
+
+}
